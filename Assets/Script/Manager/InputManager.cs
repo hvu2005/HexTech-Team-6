ae36213d0c6386f58e46ea898008cdb1ebe6f260
hvu2005 <hoangvu241205@gmail.com>
2025-02-15 01:41:47 +0700

    update package + cong nghe

---
+++ Assets/Script/Manager/InputManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Singleton<InputManager>
+{
+
+    [SerializeField] private PlayerInput data;
+    
+    public PlayerInput Data => data;
+
+    public bool CanGetAction {  get; set; }
+    public bool isJumping { get; private set; }
+    public float move { get; private set; }
+    public bool anyKeyDown { get; private set; }
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anyKeyDown = Input.anyKeyDown;
+
+        if(!CanGetAction)
+        {
+            return;
+        }
+
+        isJumping = data.actions["Jumping"].WasPressedThisFrame();
+        move = data.actions["Move"].ReadValue<float>();
+    }
+}
