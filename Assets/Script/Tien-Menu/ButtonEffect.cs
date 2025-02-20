7024a02cf7a4ed5ddd811a00b5bd05bb97dd7d93
DTien <vuongtien6200@gmail.com>
2025-02-20 22:14:46 +0700

    menu

---
+++ Assets/Script/Tien-Menu/ButtonEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public Image buttonImage; // Ảnh nền của nút
+    private Color originalColor;
+    public Color hoverColor = new Color(0.8f, 0.8f, 0.8f, 1f); // Màu khi di chuột vào
+    public Color clickColor = new Color(0.6f, 0.6f, 0.6f, 1f); // Màu khi click
+
+    void Start()
+    {
+        originalColor = buttonImage.color; // Lưu màu gốc
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.color = hoverColor; // Đổi màu khi di chuột vào
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.color = originalColor; // Trả về màu gốc khi di chuột ra
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        buttonImage.color = clickColor; // Đổi màu khi click
+        Invoke("ResetColor", 0.2f); // Reset màu sau 0.2s
+    }
+
+    void ResetColor()
+    {
+        buttonImage.color = originalColor;
+    }
+}
