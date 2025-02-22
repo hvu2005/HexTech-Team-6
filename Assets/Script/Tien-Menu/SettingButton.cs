fd2d480bf7afd270cd3d86d388fe6786775d6015
DTien <vuongtien6200@gmail.com>
2025-02-22 10:05:06 +0700

    update menu

---
+++ Assets/Script/Tien-Menu/SettingButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SettingButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+}
