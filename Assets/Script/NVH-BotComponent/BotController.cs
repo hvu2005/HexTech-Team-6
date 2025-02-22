7914cb275a1c87202a32edabf52bd7671af4da37
Monomee <hoangthth12@gmail.com>
2025-02-22 22:43:47 +0700

    add bot component

--- Assets/Script/NVH-BotComponent/BotController.cs
+++ Assets/Script/NVH-BotComponent/BotController.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BotController : MonoBehaviour
 {
     public float moveSpeed = 3f;
     public float changeDirectionTime = 1f;
     public BoxCollider2D gridArea;
-    public Vector2 moveAreaMin = new Vector2(3, -7);
-    public Vector2 moveAreaMax = new Vector2(6, -3);
+    //public Vector2 moveAreaMin = new Vector2(3, -7);
+    //public Vector2 moveAreaMax = new Vector2(6, -3);
 
     private Vector2 targetPosition;
     private Rigidbody2D rb;
@@ -17,9 +18,14 @@ public class BotController : MonoBehaviour
     public GameObject rocketPrefab;
     public Transform firePos1;
     public Transform firePos2;
+    public Transform firePos3;
     public Transform lauchPos;
+
+    public Slider slider;
+    float health;
     void Start()
     {
+        health = slider.value;
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine(ChangeDirection());
         StartCoroutine(ShootRoutine());
@@ -29,6 +35,7 @@ public class BotController : MonoBehaviour
     void Update()
     {
         Move();
+        CheckHealth();
     }
 
     void Move()
@@ -37,16 +44,26 @@ public class BotController : MonoBehaviour
         rb.MovePosition(newPosition);
     }
 
+    void CheckHealth()
+    {
+        slider.value = health;
+        if (health <= 0)
+        {
+            StopAllCoroutines();
+            //hieu ung no chi choe dung doang
+        }
+    }
+
     IEnumerator ChangeDirection()
     {
         while (true)
         {
             yield return new WaitForSeconds(changeDirectionTime);
-            float x = Random.Range(gridArea.bounds.min.x, gridArea.bounds.max.x);
+            //float x = Random.Range(gridArea.bounds.min.x, gridArea.bounds.max.x);
             float y = Random.Range(gridArea.bounds.min.y, gridArea.bounds.max.y);
             //float x = Random.Range(moveAreaMin.x, moveAreaMax.x);
             //float y = Random.Range(moveAreaMin.y, moveAreaMax.y);
-            targetPosition = new Vector2(x, y);
+            targetPosition = new Vector2(this.transform.position.x, y);
         }
     }
     IEnumerator ShootRoutine()
@@ -78,5 +95,6 @@ public class BotController : MonoBehaviour
     {
         Instantiate(bulletPrefab, firePos1.position, firePos1.rotation);
         Instantiate(bulletPrefab, firePos2.position, firePos2.rotation);
+        Instantiate(bulletPrefab, firePos3.position, firePos3.rotation);
     }
 }
