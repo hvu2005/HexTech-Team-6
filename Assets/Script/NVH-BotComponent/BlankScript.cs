6df17e99921f29e7b103229f536226035fbd972a
Monomee <hoangthth12@gmail.com>
2025-03-24 20:08:34 +0700

    Map3(no background)

---
+++ Assets/Script/NVH-BotComponent/BlankScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlankSCript : MonoBehaviour
+{
+    public List<GameObject> numList = new List<GameObject> ();
+    private int colliderCount = 0;
+    public int countMax;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject num in numList)
+        {
+            if (num.GetComponent<NumberOfPlayerOnBlank>().numbers == (countMax-colliderCount))
+            {
+                num.gameObject.SetActive(true);
+            }
+            else
+            {
+                num.gameObject.SetActive (false);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    { 
+        if (colliderCount > countMax)
+        {
+            this.gameObject.AddComponent<Rigidbody2D>();
+            this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            StartCoroutine(DestroyObject());
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+           colliderCount++;
+       // Debug.Log(colliderCount);
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+            colliderCount--;
+        // Debug.Log(colliderCount);
+        //foreach (GameObject num in numList)
+        //{
+        //    if (num.GetComponent<NumberOfPlayerOnBlank>().numbers == colliderCount)
+        //    {
+        //        num.gameObject.SetActive(true);
+        //    }
+        //}
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Script/NVH-BotComponent/TextFlashing.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextFlashing : MonoBehaviour
+{
+    public Color color1;
+    public Color color2;
+    public Text flashingText;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        FlashingText();
+    }
+
+    void FlashingText()
+    {
+        flashingText.color = Color.Lerp(color1, color2, Mathf.PingPong(Time.time, 1));
+    }
+}
