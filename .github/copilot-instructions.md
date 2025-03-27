1c4a447db8dd8340bbebde96570683badfb88f44
DTien <vuongtien6200@gmail.com>
2025-03-27 21:14:28 +0700

    Revert "Merge branch 'main' into DucTien"
    This reverts commit 1e60e368e63006c7b919bb9342a74ba0d4c37c6b, reversing
    changes made to e912baec980810aee7560fea003aefaf3b135fe6.

--- .github/copilot-instructions.md
+++
@@ -1,4 +0,0 @@
-<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
-- Project name: HexTech-Team-6
-- Unity version: Unity 2022.3.45f1
-<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Script/HVU-Player/Player.cs
+++ Assets/Script/HVU-Player/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
@@ -43,43 +43,39 @@ public class Player : NetworkBehaviour
     {
         if (!IsServer) return;
 
+
         Flip();
         checkIsGrounded();
 
-        // Cập nhật tốc độ di chuyển
-        Rb.velocity = new Vector2(_controller.xMove * moveSpeed, Rb.velocity.y);
+        Rb.velocity = new Vector2(_controller.move * moveSpeed, Rb.velocity.y);
 
-        // Nhảy
-        if (isGrounded && _controller.isJumping)
+        if(isGrounded && _controller.isJumping)
         {
             Rb.velocity = new Vector2(Rb.velocity.x, jumpForce);
-            isGrounded = false; // Ngăn nhảy liên tục
         }
     }
-
 
     private void checkIsGrounded()
     {
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + groundOffset, Vector2.down, distance, whatIsGround);
-        Debug.DrawRay((Vector2)transform.position + groundOffset, Vector2.down * distance, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + groundOffset, Vector2.left, distance, whatIsGround );
+        Debug.DrawRay((Vector2)transform.position + groundOffset, Vector2.left * distance, Color.red);
         isGrounded = hit.collider != null;
     }
-
-
+    
     private void Flip()
     {
-        if (!CanMove) return;
-
-        if (_controller.xMove > 0 && !isFacingRight)
+        if(CanMove)
         {
-            isFacingRight = true;
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (_controller.xMove < 0 && isFacingRight)
-        {
-            isFacingRight = false;
-            transform.localScale = new Vector3(-1, 1, 1);
+            if(_controller.move > 0 && !isFacingRight)
+            {
+                isFacingRight = !isFacingRight;
+                transform.rotation = Quaternion.identity;
+            }
+            else if(_controller.move < 0 && isFacingRight)
+            {
+                isFacingRight = !isFacingRight;
+                transform.rotation = Quaternion.Euler(0, -180, 0);
+            }
         }
     }
-
 }
