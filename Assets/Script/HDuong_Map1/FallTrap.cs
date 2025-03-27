485e9b7a7172bdb77511d38cca5092ab056aad08
HoangOcean99 <duonghaiduong090905@gmail.com>
2025-03-28 05:50:00 +0700

    UpdateOnlineMap1Map2

---
+++ Assets/Script/HDuong_Map1/FallTrap.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class FallTrap : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool hasFallen = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+
+        rb.bodyType = RigidbodyType2D.Kinematic; // Để bẫy không rơi ngay
+        gameObject.SetActive(false); // Ẩn bẫy ngay từ đầu
+
+        Debug.Log("FallTrap Initialized - Hidden");
+    }
+
+    public void DropTrap()
+    {
+        if (!hasFallen)
+        {
+            gameObject.SetActive(true); // Hiện bẫy ra
+            rb.bodyType = RigidbodyType2D.Dynamic; // Cho phép rơi xuống
+            rb.gravityScale = 2f;
+            hasFallen = true;
+
+            Debug.Log("Trap Activated - Falling!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasFallen)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic; // Khi chạm đất thì đứng yên
+            Debug.Log("Trap Landed - Now Static");
+        }
+    }
+    
+}
---
+++ Assets/Script/HDuong_Map1/TrapTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TrapTrigger : MonoBehaviour
+{
+    public FallTrap trap;
+    private PlayerControl playerController;
+    private void Start()
+    {
+        playerController = FindAnyObjectByType<PlayerControl>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger - Activating Trap!");
+            trap.DropTrap();
+            Destroy(gameObject); // Xóa vùng kích hoạt sau khi bẫy đã rơi
+        }
+    }
+}
