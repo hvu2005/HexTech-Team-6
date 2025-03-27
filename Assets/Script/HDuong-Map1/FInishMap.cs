4f990b69bf45ae74d944bb734e06f3532e461127
HoangOcean99 <duonghaiduong090905@gmail.com>
2025-03-27 23:55:43 +0700

    UpdateOnline

---
+++ Assets/Script/HDuong-Map1/FInishMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class FinishMap : NetworkBehaviour
+{
+    public GameObject buttonNextMap;
+
+    public int colliderCount = 0;
+    public int countMax;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("1");
+        Debug.Log(colliderCount+"");
+        colliderCount++;
+        Debug.Log("2");
+        // 🟢 Kiểm tra nếu đủ số người chơi → Hiện button
+        if (colliderCount == countMax && IsServer)
+        {
+            buttonNextMap.SetActive(true);
+        }
+        Debug.Log(colliderCount + "");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        colliderCount--;
+    }
+}
