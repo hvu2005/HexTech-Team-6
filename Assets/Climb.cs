76b2c22810e06aff9cc33603d278d7864b5d7e34
hvu2005 <hoangvu241205@gmail.com>
2025-03-29 06:39:13 +0700

    update map 1

--- Assets/Climb.cs
+++ Assets/Climb.cs
@@ -1,63 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class PlayerClimb : MonoBehaviour
 {
-    Rigidbody2D rb;
-    float defaultGravityScale;
 
     float vertical;
-    bool canClimb;
     [SerializeField] float climbSpeed;
     // Start is called before the first frame update
     void Awake()
     {
-        canClimb = false;
-        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        defaultGravityScale = rb.gravityScale;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Climb();
         //rb.AddForce(Vector2.up, ForceMode2D.Impulse);
     }
 
-    void Climb()
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (canClimb)
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (other != null && other.CompareTag("Player"))
             {
+                PlayerControl player = other.gameObject.GetComponent<PlayerControl>();
+                player.Rb.gravityScale = 0f;
                 vertical = Input.GetAxis("Vertical");
-                rb.gravityScale = 0;
-                rb.velocity = new Vector2(rb.velocity.x, vertical * climbSpeed);
-                //Instantiate()
+                if (vertical > 0f)
+                {
+                    //rb.velocity = new Vector2(rb.velocity.x, vertical * climbSpeed);
+                    //Instantiate()
+                    player.Rb.velocity = new Vector2(player.Rb.velocity.x, climbSpeed);
+                }
+                else if (vertical < 0f)
+                {
+                    player.Rb.velocity = new Vector2(player.Rb.velocity.x, -climbSpeed);
+                }
+                else
+                {
+                    player.Rb.velocity = new Vector2(player.Rb.velocity.x, 0f);
+                }
             }
-        }
-        else
-        {
-            rb.gravityScale = defaultGravityScale;
-        }
+            //else
+            //{
+            //    rb.gravityScale = defaultGravityScale;
+            //}
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other != null && other.CompareTag(rb.gameObject.tag))
         {
-            canClimb = true;
-            //Debug.Log("CanClimb: "+ canClimb);
+            //Player player = other.gameObject.GetComponent<Player>();
+            //PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+
+
+            //else if (playerController.yMove < 0f)
+            //{
+            //    player.Rb.velocity = new Vector2(player.Rb.velocity.x, -climbSpeed);
+            //}
+            //else
+            //{
+            //    player.Rb.velocity = new Vector2(player.Rb.velocity.x, 0f);
+            //}
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other != null && other.CompareTag(rb.gameObject.tag))
-        {
-            canClimb = false;
-            //Debug.Log("CanClimb: " + canClimb);
-        }
+        if (other != null && other.CompareTag("Player"))
+            if (other != null && other.CompareTag("Player"))
+            {
+                //Debug.Log("CanClimb: " + canClimb);
+                PlayerControl player = other.gameObject.GetComponent<PlayerControl>();
+                player.Rb.gravityScale = 3f;
+                player.Rb.velocity = new Vector2(player.Rb.velocity.x, player.Rb.velocity.y);
+
+
+            }
     }
 }
